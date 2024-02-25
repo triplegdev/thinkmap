6b25e815198106bf25b019a12f77f0d8a872a45d
Anthony Garcia <triplegdev@gmail.com>
2024-02-25 06:16:57 -0500

    converted code using fabricjs and added text editing

--- react-vite/src/components/Symbol/Symbol.jsx
+++ react-vite/src/components/Symbol/Symbol.jsx
@@ -1,51 +1,185 @@
+import { fabric } from 'fabric';
 const SYMBOL_SIZE = 150;
 
 const Symbol = {
-  draw: (ctx, type, x, y, text) => {
-    switch (type) {
+  draw: function(canvas, id, type, x, y, text, onEditSymbol) { // Accept canvas as the first argument
+    let shape;
+    let textObj;
+    let points;
+      switch (type) {
         case 'Terminal':
-          // Draw ellipse
-          ctx.beginPath();
-          ctx.ellipse(x, y, SYMBOL_SIZE / 2, SYMBOL_SIZE / 4, 0, 0, Math.PI * 2);
-          ctx.fillStyle = '#E2937B';
-          ctx.fill();
+          shape = new fabric.Ellipse({
+              left: x,
+              top: y,
+              rx: SYMBOL_SIZE / 2,
+              ry: SYMBOL_SIZE / 4,
+              fill: '#E2937B',
+          });
+          textObj = new fabric.Textbox(text, {
+            left: x + SYMBOL_SIZE / 2,
+            top: y + SYMBOL_SIZE / 4,
+            originX: 'center',
+            originY: 'center',
+            fill: 'black',
+            fontSize: 12,
+            fontFamily: 'Arial',
+            textAlign: 'center',
+            textBaseline: 'middle',
+            editable: true, // Enable editing
+          });
           break;
-        case 'Decision':
-          // Draw diamond
-          ctx.beginPath();
-          ctx.moveTo(x, y - SYMBOL_SIZE / 3);
-          ctx.lineTo(x + SYMBOL_SIZE / 1.75, y);
-          ctx.lineTo(x, y + SYMBOL_SIZE / 3);
-          ctx.lineTo(x - SYMBOL_SIZE / 1.75, y);
-          ctx.closePath();
-          ctx.fillStyle = '#E6B85F';
-          ctx.fill();
-          break;
-        case 'Process':
-          // Draw rectangle
-          ctx.fillStyle = '#61C16A';
-          ctx.fillRect(x - SYMBOL_SIZE / 2, y - SYMBOL_SIZE / 4, SYMBOL_SIZE, SYMBOL_SIZE / 2);
-          break;
-        case 'Data':
-          ctx.beginPath();
-          ctx.moveTo(x - SYMBOL_SIZE / 2, y - SYMBOL_SIZE / 4);
-          ctx.lineTo(x + SYMBOL_SIZE / 2, y - SYMBOL_SIZE / 4);
-          ctx.lineTo(x + SYMBOL_SIZE / 1.33, y + SYMBOL_SIZE / 4);
-          ctx.lineTo(x - SYMBOL_SIZE / 4, y + SYMBOL_SIZE / 4)
-          ctx.closePath();
-          ctx.fillStyle = '#7CABE2';
-          ctx.fill();
-          break;
-        default:
-          break;
-    }
+          case 'Decision':
+              // Draw diamond using Fabric.js methods
+              shape = new fabric.Polygon([
+                  { x: x + SYMBOL_SIZE / 1.75, y },
+                  { x: x + SYMBOL_SIZE / 0.875, y: y + SYMBOL_SIZE / 3 },
+                  { x: x + SYMBOL_SIZE / 1.75, y: y + SYMBOL_SIZE / 1.5 },
+                  { x, y: y + SYMBOL_SIZE / 3 },
+              ], {
+                  fill: '#E6B85F',
+              });
+              textObj = new fabric.Textbox(text, {
+                left: x + SYMBOL_SIZE / 1.75,
+                top: y + SYMBOL_SIZE / 3,
+                originX: 'center',
+                originY: 'center',
+                fill: 'black',
+                fontSize: 12,
+                fontFamily: 'Arial',
+                textAlign: 'center',
+                textBaseline: 'middle',
+                editable: true, // Enable editing
+              });
+            break;
+          case 'Process':
+              // Draw rectangle using Fabric.js methods
+              shape = new fabric.Rect({
+                  left: x,
+                  top: y,
+                  width: SYMBOL_SIZE,
+                  height: SYMBOL_SIZE / 2,
+                  fill: '#61C16A',
+              });
+              textObj = new fabric.Textbox(text, {
+                left: x + SYMBOL_SIZE / 2,
+                top: y + SYMBOL_SIZE / 4,
+                originX: 'center',
+                originY: 'center',
+                fill: 'black',
+                fontSize: 12,
+                fontFamily: 'Arial',
+                textAlign: 'center',
+                textBaseline: 'middle',
+                editable: true, // Enable editing
+              });
+            break;
+          case 'Data':
+              // Draw polygon using Fabric.js methods
+              points = [
+                  { x, y },
+                  { x: x + SYMBOL_SIZE, y},
+                  { x: x + SYMBOL_SIZE * 1.25, y: y + SYMBOL_SIZE / 2},
+                  { x: x + SYMBOL_SIZE / 4, y: y + SYMBOL_SIZE / 2 }
+              ];
+              shape = new fabric.Polygon(points, {
+                  fill: '#7CABE2',
+              });
+              textObj = new fabric.Textbox(text, {
+                left: x + SYMBOL_SIZE / 1.625,
+                top: y + SYMBOL_SIZE / 4,
+                originX: 'center',
+                originY: 'center',
+                fill: 'black',
+                fontSize: 12,
+                fontFamily: 'Arial',
+                textAlign: 'center',
+                textBaseline: 'middle',
+                editable: true, // Enable editing
+              });
+            break;
+          default:
+              break;
+      }
 
-    ctx.fillStyle = 'black';
-    ctx.font = '12px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText(text, x, y);
-  }
+      textObj.width = SYMBOL_SIZE
+
+      const group = new fabric.Group([shape, textObj], {
+        left: shape.left,
+        top: shape.top,
+        originX: 'center',
+        originY: 'center',
+        id,
+        symbolType: type
+      });
+
+      group.on('mousedblclick', () => {
+        // textForEditing is temporary obj,
+        // and will be removed after editing
+        let textForEditing = new fabric.Textbox(textObj.text, {
+          originX: 'center',
+          originY: 'center',
+          textAlign: textObj.textAlign,
+          fontSize: textObj.fontSize,
+          fontFamily: textObj.fontFamily,
+          left: group.left,
+          top: group.top,
+          width: textObj.width
+        })
+
+        // hide group inside text
+        textObj.visible = false;
+        // note important, text cannot be hidden without this
+        group.addWithUpdate();
+
+        textForEditing.visible = true;
+        // do not give controls, do not allow move/resize/rotation on this
+        textForEditing.hasConstrols = false;
+
+
+        // now add this temporary obj to canvas
+        canvas.add(textForEditing);
+        canvas.setActiveObject(textForEditing);
+        // make the cursor showing
+        textForEditing.enterEditing();
+        textForEditing.selectAll();
+
+
+        // editing:exited means you click outside of the textForEditing
+        textForEditing.on('editing:exited', () =>{
+            let newVal = textForEditing.text;
+            let oldVal = text.text;
+            // console.log(textObj.text)
+
+          // then we check if text is changed
+          if (newVal !== oldVal) {
+            textObj.set({
+                text: newVal,
+                visible: true,
+            })
+
+            // comment before, you must call this
+            group.addWithUpdate();
+
+            // we do not need textForEditing anymore
+            textForEditing.visible = false;
+            canvas.remove(textForEditing);
+
+            // optional, buf for better user experience
+            canvas.setActiveObject(group);
+            onEditSymbol({
+                x_position: x,
+                y_position: y,
+                text: textObj.text,
+                type: group.symbolType
+            }, group.id);
+          }
+        })
+      })
+
+      // group.addWithUpdate();
+      canvas.add(group);
+    },
+
 };
 
 export default Symbol;
